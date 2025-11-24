749122f0daa0f4198721a1ae38fdd2493b52c7a4
ABodyan <a.bodyan@web-modern.com>
2025-11-24 21:58:13 +0400

    add commit

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -16,4 +16,8 @@ describe('Cypress Test Cy', () => {
         cy.get('input[name="q"]').type('Cypress');
         cy.get('input[name="q"]').type('{enter}');
     })
+    it('should be able to search for a word', () => {
+        cy.get('input[name="q"]').type('Cypress');
+        cy.get('input[name="q"]').type('{enter}');
+    })  
 })
